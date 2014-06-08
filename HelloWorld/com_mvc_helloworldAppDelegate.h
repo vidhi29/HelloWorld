02e0a9ffd6b14c938de91fdee6887b4efdcdff79
vidhi bhagdev <vidhibhagdev@vidhis-MacBook-Pro.local>
2014-06-07 18:50:04 -0700

    Initial Commit

---
+++ HelloWorld/com_mvc_helloworldAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  com_mvc_helloworldAppDelegate.h
+//  HelloWorld
+//
+//  Created by vidhi bhagdev on 6/7/14.
+//  Copyright (c) 2014 MVC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface com_mvc_helloworldAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by vidhi bhagdev on 6/7/14.
+//  Copyright (c) 2014 MVC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "com_mvc_helloworldAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([com_mvc_helloworldAppDelegate class]));
+    }
+}
